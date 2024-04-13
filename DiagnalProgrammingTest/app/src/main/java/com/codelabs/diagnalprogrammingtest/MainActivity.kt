0e3daec761891334a289a2a28acaac621247a6fd
nagarajudbng <nagaraj.dbng@gmail.com>
2024-04-14 01:43:42 +0530

    OfflineCache

--- DiagnalProgrammingTest/app/src/main/java/com/codelabs/diagnalprogrammingtest/MainActivity.kt
+++ DiagnalProgrammingTest/app/src/main/java/com/codelabs/diagnalprogrammingtest/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.codelabs.diagnalprogrammingtest.ui.theme.DiagnalProgrammingTestTheme
-
+//Test OfflineCache
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
